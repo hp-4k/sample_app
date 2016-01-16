ac3154952524e6cfe67f855d8864108f96ec807e
Hubert <hpompecki@gmail.com>
2016-01-16 12:46:59 +0000

    Solve Chapter 9 exercises

--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -33,6 +33,12 @@ class UserEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name, @user.name
     assert_equal email, @user.email
+    
+    delete logout_path
+    get login_path
+    log_in_as(@user)
+    assert session[:forwarding_url].nil?
+    assert_redirected_to user_url(@user)
   end
   
 end
